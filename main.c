adb2d8c530f7453ae1fa0b5ed42b5de6af48fcd7
Ankit Ydv <ankityadav11241@gmail.com>
2025-06-26 19:18:45 +0530

    entry function

---
+++ main.c
@@ -0,0 +1,12 @@
+#include "socket.c"
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    printf("Usage: %s <port>\n", argv[0]);
+    return 1;
+  }
+  int port = atoi(argv[1]);
+  initializeSocket(port);
+
+  return 0;
+}
