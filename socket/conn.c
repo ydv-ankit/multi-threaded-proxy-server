80044471fe7fca0accb74a49fd6598e3083bf45a
Ankit Ydv <ankityadav11241@gmail.com>
2025-06-27 13:05:14 +0530

    dynamically accept complete request

--- socket/conn.c
+++ socket/conn.c
@@ -3,24 +3,50 @@
 #include <stdlib.h>
 #include <sys/socket.h>
 
-#define BUFFER_SIZE 1024 * 512 // 512 KB
+#define BUFFER_SIZE 1024 * 64 // 64 KB
+
+struct ClientData {
+  int client_fd;
+  char *buffer;
+  size_t current_size;
+  size_t capacity;
+  int is_complete;
+};
+
+void *expandMemory(void *ptr, int exp_size) {
+  void *tmp = (void *)realloc(ptr, exp_size);
+  if (tmp == NULL) {
+    errorLog("error while memory expansion");
+    return ptr;
+  }
+  return tmp;
+}
 
 void handleClient(int c_fd) {
   char *buffer = (char *)malloc(BUFFER_SIZE);
+  int bytes_count = 1;
+  size_t curr_ptr = 0;
   while (1) {
-    int bytes_read = recv(c_fd, buffer, sizeof(buffer), 0);
-    infoLog("bytes: %d", bytes_read);
+    int bytes_read = recv(c_fd, buffer + curr_ptr, BUFFER_SIZE, 0);
+    infoLog("bytes: %d\n", bytes_read);
     if (bytes_read == 0) {
       infoLog("connection closed by client");
-      return;
+      break;
     } else if (bytes_read == -1) {
       errorLog("error reading client request");
-      return;
+      break;
     } else if (bytes_read > 0) {
-      buffer[bytes_read] = '\0';
-      printf("client [%d]: %s\n", c_fd, buffer);
+      if (bytes_read == BUFFER_SIZE) {
+        buffer = expandMemory(buffer, (++bytes_count * BUFFER_SIZE));
+        curr_ptr += bytes_read;
+        continue;
+      }
+      curr_ptr += bytes_read;
+      buffer[curr_ptr + 1] = '\0';
+      printf("client [%d]: %s", c_fd, buffer);
     } else {
       errorLog("unknown error occured");
     }
   }
+  free(buffer);
 }
